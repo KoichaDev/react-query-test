c9075f4ab25d3466dd849a43dbe2dd3b56d17af0
“Khoi <khoi@godtsagt.no>
2021-12-16 21:04:04 +0100

    Add: ✨ state error for loading API from super heroes page site

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -1,32 +1,43 @@
-import { useState, useEffect, useContext } from 'react'
+import { useState, useEffect, useContext } from 'react';
 import endPointContext from 'store/end-points-context';
-import axios from 'axios'
+import axios from 'axios';
 
 export const SuperHeroesPage = () => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [data, setData] = useState([])
+	const [isLoading, setIsLoading] = useState(true);
+	const [data, setData] = useState([]);
+	const [error, setError] = useState('');
 
-  const endPointCtx = useContext(endPointContext)
+	const endPointCtx = useContext(endPointContext);
 
-  const url = endPointCtx.superHeroes;
+	const url = endPointCtx.superHeroes;
 
-  useEffect(() => {
-    axios.get(url).then(res => {
-      setData(res.data)
-      setIsLoading(false)
-    })
-  }, [])
+	useEffect(() => {
+		axios
+			.get(url)
+			.then((res) => {
+				setData(res.data);
+				setIsLoading(false);
+			})
+			.catch((error) => {
+				setError(error);
+				setIsLoading(false);
+			});
+	}, []);
 
-  if (isLoading) {
-    return <h2>Loading...</h2>
-  }
+	if (isLoading) {
+		return <h2>Loading...</h2>;
+	}
 
-  return (
-    <>
-      <h2>Super Heroes Page</h2>
-      {data.map(hero => {
-        return <div>{hero.name}</div>
-      })}
-    </>
-  )
-}
+	if (error) {
+		return <h2>{error}</h2>;
+	}
+
+	return (
+		<>
+			<h2>Super Heroes Page</h2>
+			{data.map((hero) => {
+				return <div>{hero.name}</div>;
+			})}
+		</>
+	);
+};
