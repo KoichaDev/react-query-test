a39ecd2bcf55ea996a0e0c63c2053f722fa09be0
“Khoi <khoi@godtsagt.no>
2021-12-17 04:11:43 +0100

    Add: ✨ success or error callback for fetching the data

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,6 +2,14 @@ import { useQuery } from 'react-query';
 import fetchSuperHeroes from './services/fetch-super-heroes';
 
 export const RQSuperHeroesPage = () => {
+	const onSuccess = (data) => {
+		console.log('Perfom side effects after data fetching', data);
+	}
+
+	const onError = (error) => {
+		console.log('Perfom side effects after encountering error', error);
+	}
+
 	const { getData } = fetchSuperHeroes();
 	// Here, we don't need to manage the state variables and using the useEffect
 	const {
@@ -16,12 +24,12 @@ export const RQSuperHeroesPage = () => {
 		// staleTime: 30000, // This is to "stale the time" for how long it will display on the site. In thi case it's 30 seconds. Good if the data doesn't change often on the site. Default value is 0 seconds
 		// refetchOnMount: false, // This will refetch on mount if the data is "stale". This is the default value is true. Adding the false, the data will not refetch
 		// refetchOnWindowFocus: true, // every time your window tab loses focus and gains focus again, a background refetch  is initiated. Default value is set to true
-		refetchInterval: 2000, // This will always refetch every 2 seconds. useful is we want for example get data from stock price. default is set to false
-		refetchIntervalInBackground: true, // this will continue to pull data even the browser even if the windows loose the focus. This provides a really good user experience where data changes every now and then
-		enabled: false, // Inform react query not to fire the get request when the component mounts
+		// refetchInterval: 2000, // This will always refetch every 2 seconds. useful is we want for example get data from stock price. default is set to false
+		// refetchIntervalInBackground: true, // this will continue to pull data even the browser even if the windows loose the focus. This provides a really good user experience where data changes every now and then
+		// enabled: false, // Inform react query not to fire the get request when the component mounts,
+		onSuccess,
+		onError,
 	});
-
-	console.log({ isLoading, isFetching });
 
 	if (isLoading || isFetching) {
 		return <h2>Loading...</h2>;
