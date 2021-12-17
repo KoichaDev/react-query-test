48bbeb4fca87022d5bbd9c79419935f43c050412
“Khoi <khoi@godtsagt.no>
2021-12-17 03:58:58 +0100

    Add: ✨ Manipulating by Pulling the data interval settings

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -15,6 +15,8 @@ export const RQSuperHeroesPage = () => {
 		// staleTime: 30000, // This is to "stale the time" for how long it will display on the site. In thi case it's 30 seconds. Good if the data doesn't change often on the site. Default value is 0 seconds
 		// refetchOnMount: false, // This will refetch on mount if the data is "stale". This is the default value is true. Adding the false, the data will not refetch
 		// refetchOnWindowFocus: true, // every time your window tab loses focus and gains focus again, a background refetch  is initiated. Default value is set to true
+		refetchInterval: 2000, // This will always refetch every 2 seconds. useful is we want for example get data from stock price. default is set to false
+		refetchIntervalInBackground: true, // this will continue to pull data even the browser even if the windows loose the focus. This provides a really good user experience where data changes every now and then
 	});
 
 	console.log({isLoading, isFetching});
