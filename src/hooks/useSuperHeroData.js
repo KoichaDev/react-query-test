8d5846e890b76d2e7905e3b14f7942f8210c40f2
“Khoi <khoi@godtsagt.no>
2021-12-17 10:00:32 +0100

    Add: ✨ Initial query data for partial downloading the data on the site
    If you fetching from a page with whole list and when you are going
    to visit another page, we are using the previous whole list
    and getting the specific ID to show the data to new page from our query.
    This improves the UX a slightly better because we are removing the
    "loading" on the site

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,10 +1,28 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import fetchSuperHeroes from '../services/fetch-super-heroes';
 
 const useSuperHeroData = (heroId) => {
-	const { getDataById  } = fetchSuperHeroes();
+	const { getDataById } = fetchSuperHeroes();
 
-	return useQuery(['super-hero', heroId], () => getDataById(heroId));
+	const queryClient = useQueryClient();
+
+	return useQuery(['super-hero', heroId], () => getDataById(heroId), {
+		initialData: () => {
+			// Getting the data from the "super heroes"
+			const hero = queryClient
+				.getQueryData('super-heroes')
+				?.data?.find((hero) => hero.id === parseInt(heroId));
+
+			if (hero) {
+				return { 
+					data: hero 
+				};
+			} else  {
+				// By returning it, will save us from runtime error, otherwise we will be trying to access the "data.data.name" when it doesn't exist yet
+				return undefined;
+			}
+		},
+	});
 };
 
 export default useSuperHeroData;
